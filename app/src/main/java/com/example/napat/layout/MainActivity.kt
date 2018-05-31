cb971bfa274ac781f355b6c266945d500efe1a15
Kran <napatc58@email.nu.ac.th>
2018-05-31 12:40:29 +0700

    add another layout

--- app/src/main/java/com/example/napat/layout/MainActivity.kt
+++ app/src/main/java/com/example/napat/layout/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.napat.layout
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
 
@@ -9,10 +10,17 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
-    fun on_bt_Fream_layout(){
+
+    fun on_bt_Fream_layout(view: View){
         setContentView(R.layout.frame_layout)
     }
-    fun Back_click(){
+    fun Back_click(view: View){
+        setContentView(R.layout.activity_main)
+    }
+    fun bt_Linear_layout(view: View){
+        setContentView(R.layout.activity_main)
+    }
+    fun bt_Relative_layout(view: View){
         setContentView(R.layout.activity_main)
     }
 }
