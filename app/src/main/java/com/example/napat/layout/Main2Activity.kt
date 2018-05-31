dd62386f33740fe3ce999ac61a60d2c9377b114f
Kran <napatc58@email.nu.ac.th>
2018-06-01 03:19:26 +0700

    Updeat recycle view

---
+++ app/src/main/java/com/example/napat/layout/Main2Activity.kt
@@ -0,0 +1,60 @@
+package com.example.napat.layout
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main2.*
+
+class Main2Activity : AppCompatActivity() {
+    val animals : ArrayList<String> = ArrayList()
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2)
+        addAnimals()
+        rv_animal_list.layoutManager = LinearLayoutManager(this)
+        rv_animal_list.adapter = ItemAdater(animals, this)
+    }
+        fun bt_back(view: View){
+            val intent = Intent(this,MainActivity::class.java)
+            startActivity(intent)
+        }
+
+        fun addAnimals() {
+            animals.add("dog")
+            animals.add("cat")
+            animals.add("owl")
+            animals.add("cheetah")
+            animals.add("raccoon")
+            animals.add("bird")
+            animals.add("snake")
+            animals.add("lizard")
+            animals.add("hamster")
+            animals.add("bear")
+            animals.add("lion")
+            animals.add("tiger")
+            animals.add("horse")
+            animals.add("frog")
+            animals.add("fish")
+            animals.add("shark")
+            animals.add("turtle")
+            animals.add("elephant")
+            animals.add("cow")
+            animals.add("beaver")
+            animals.add("bison")
+            animals.add("porcupine")
+            animals.add("rat")
+            animals.add("mouse")
+            animals.add("goose")
+            animals.add("deer")
+            animals.add("fox")
+            animals.add("moose")
+            animals.add("buffalo")
+            animals.add("monkey")
+            animals.add("penguin")
+            animals.add("parrot")
+        }
+
+    }
+
--- app/src/main/java/com/example/napat/layout/MainActivity.kt
+++ app/src/main/java/com/example/napat/layout/MainActivity.kt
@@ -1,7 +1,8 @@
 package com.example.napat.layout
 
-import android.support.v7.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 import android.view.View
 
 class MainActivity : AppCompatActivity() {
@@ -9,18 +10,31 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+    }
+    fun bt_Recycelview_layout(view: View){
+        val intent = Intent(this,Main2Activity::class.java)
+        startActivity(intent)
     }
 
-    fun on_bt_Fream_layout(view: View){
+    fun on_bt_Fream_layout(view:View) {
         setContentView(R.layout.frame_layout)
     }
-    fun Back_click(view: View){
+    fun Back_click(view:View) {
         setContentView(R.layout.activity_main)
     }
-    fun bt_Linear_layout(view: View){
+    fun bt_Linear_layout(view:View) {
         setContentView(R.layout.linear_layout)
     }
-    fun bt_Relative_layout(view: View){
+    fun bt_Relative_layout(view: View) {
         setContentView(R.layout.relative_layout)
     }
-}
+    fun run (){
+        setContentView(R.layout.activity_main)
+    }
+
+
+
+    }
+
